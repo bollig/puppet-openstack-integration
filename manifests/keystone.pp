8308ee84b43c698ccbc746ba6236c7055f8fe64c
Evan Bollig <bollig@gmail.com>
2016-01-13 04:25:45 -0600

    Cleanup

--- manifests/keystone.pp
+++ manifests/keystone.pp
@@ -4,35 +4,27 @@ class openstack_integration::keystone {
   class { '::keystone::cron::token_flush': }
   class { '::keystone::db::mysql':
     password => 'keystone',
-    #allowed_hosts => [ '127.0.0.1', 'localhost' ],
   }
   class { '::keystone':
     verbose             => true,
     debug               => true,
     database_connection => "mysql+pymysql://keystone:keystone@127.0.0.1/keystone",
     admin_token         => 'admin_token',
-# EVAN: disabled for ssl support through wsgi
+# EVAN: control the service
     enabled             => true,
     service_name        => 'httpd',
-	# Evan: 
+# Evan: the keystone api runs behind Apache. no need to enable here
+# since its enabled in the keystone::wsgi::apache class
     enable_ssl 		=> false,
     public_endpoint     => 'https://localhost:5000',
     admin_endpoint     => 'https://localhost:35357',
-	# These come from the packstack generated cert
-    #ssl_cert_subject => '/C=--/ST=State/L=City/O=openstack/OU=packstack/CN=localhost.localdomain/E=admin@localhost.localdomain',
-    #ssl_certfile        => "/etc/pki/tls/certs/ssl_dashboard.crt",
-    #ssl_keyfile         => "/etc/pki/tls/private/ssl_dashboard.key",
-    #ssl_ca_key          => "/etc/pki/tls/certs/packstack_cacert.crt",
-	# NEcessary until we have a CA signed cert
-    #validate_insecure   => true,
-    #default_domain => 'Default',
   }
 
   include ::apache
   class { '::keystone::wsgi::apache':
-	# Evan: 
+	# Evan: Enable SSL; remember to set the endpoints
     ssl     => true,
-#    #workers => 2,
+    workers => 2,
     ssl_cert        => "/etc/pki/tls/certs/ssl_dashboard.crt",
     ssl_key         => "/etc/pki/tls/private/ssl_dashboard.key",
     ssl_ca          => "/etc/pki/tls/certs/packstack_cacert.crt",
@@ -42,19 +34,19 @@ class openstack_integration::keystone {
     password => 'a_big_secret',
   }
   class { '::keystone::endpoint':
- #   default_domain => 'admin',
-  #  # Evan: This way we force the hostname to match the cert
+# Evan: Adjust these endpoints to match the hostname in the cert
     admin_url => "https://127.0.0.1:35357/",
     public_url => "https://127.0.0.1:5000/",
   }
 
+# EVAN: the following configure settings in conf files (e.g.,
+# glance_registry_config ==> /etc/glance/glance_registry.conf; and vars are
+# 'section/setting' ==> [section]\nsetting ) 
    glance_registry_config {
 	 'keystone_authtoken/insecure': value => true; 
-	 #'keystone_authtoken/identity_uri': value => 'https://127.0.0.1:35357/'; 
    }
    glance_api_config {
 	 'keystone_authtoken/insecure': value => true; 
-	 #'keystone_authtoken/identity_uri': value => 'https://127.0.0.1:35357/'; 
    }
    neutron_config {
 	 'keystone_authtoken/insecure': value => true; 
@@ -82,20 +74,4 @@ class openstack_integration::keystone {
 	 'filter:authtoken/insecure': value => true; 
    }
 
-    #keystone::resource::authtoken { 'neutron_api_config':
-#	username => 'admin', 
-#	password => 'a_big_secret',
-#	auth_url => 'https://localhost:35357/',
-#	insecure => true,
-#	project_name => 'admin', 
-#	default_domain_name => 'admin', 
-#     }
-#    keystone::resource::authtoken { 'glance_api_config':
-#	username => 'admin', 
-#	password => 'a_big_secret',
-#	auth_url => 'https://localhost:35357/',
-#	insecure => true,
-#	project_name => 'admin', 
-#	default_domain_name => 'admin', 
-#     }
 }
